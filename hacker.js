43b7fc479bb22282aaf3aedfa99468109f005fba
ErenRubiks <mangadiyari1@gmail.com>
2025-02-15 23:34:28 +0300

    Update hacker.js

--- hacker.js
+++ hacker.js
@@ -1,12 +1,33 @@
 const hackerText = document.getElementById("hacker-text");
 
 let messages = [
-    "Bağlantı Kuruluyor...",
-    "Sisteme Erişim Sağlandı!",
-    "Telefon Bilgileri Çekiliyor...",
-    "Dosyalar Siliniyor...",
-    "Cihaz Formatlanıyor...",
-    "FORMAT TAMAMLANDI."
+    "Hacklendiniz :)",
+    "Cihaza Bağlantı Kuruluyor...",
+    "Telefon Bilgileri Çalınıyor %0",
+    "Telefon Bilgileri Çalınıyor %10",
+    "Telefon Bilgileri Çalınıyor %20",
+    "Telefon Bilgileri Çalınıyor %30",
+    "Telefon Bilgileri Çalınıyor %40",
+    "Telefon Bilgileri Çalınıyor %50",
+    "Telefon Bilgileri Çalınıyor %60",
+    "Telefon Bilgileri Çalınıyor %70",
+    "Telefon Bilgileri Çalınıyor %80",
+    "Telefon Bilgileri Çalınıyor %90",
+    "Telefon Bilgileri Çalınıyor %100",
+    "Telefon Bilgileri Başarıyla Çalındı",
+    "Dosya Bilgiler Çalınıyor %0",
+    "Dosya Bilgileri Çalınıyor %0",
+    "Dosya Bilgileri Çalınıyor %10",
+    "Dosya Bilgileri Çalınıyor %20",
+    "Dosya Bilgileri Çalınıyor %30",
+    "Dosya Bilgileri Çalınıyor %40",
+    "Dosya Bilgileri Çalınıyor %50",
+    "Dosya Bilgileri Çalınıyor %60",
+    "Dosya Bilgileri Çalınıyor %70",
+    "Dosya Bilgileri Çalınıyor %80",
+    "Dosya Bilgileri Çalınıyor %90",
+    "Dosya Bilgileri Çalınıyor %100",
+    "Dosya Bilgileri Başarıyla Çalındı",
 ];
 
 let index = 0;
